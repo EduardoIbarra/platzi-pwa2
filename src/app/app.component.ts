5a2fc485f9624d83387283ae6d915f9ae4a2a089
Eduardo <ibredu@gmail.com>
2018-03-23 19:30:17 -0600

    Added version code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor(private swUpdate: SwUpdate){
+  }
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe((v) => {
+        if (confirm('Actualización disponible, deseas obtenerla?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
