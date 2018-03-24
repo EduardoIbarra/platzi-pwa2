e152a977340c7fed21fb2b246b4787123d94e402
Eduardo <ibredu@gmail.com>
2018-03-24 09:34:34 -0600

    Added angular material integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,11 @@ import { AppComponent } from './app.component';
 
 import { environment } from '../environments/environment';
 import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {
+  MatExpansionModule, MatFormFieldModule, MatInputModule,
+  MatToolbarModule
+} from '@angular/material';
 
 @NgModule({
   declarations: [
@@ -15,7 +20,12 @@ import {HttpClientModule} from '@angular/common/http';
   imports: [
     BrowserModule,
     ServiceWorkerModule.register('/ngsw-worker.js', { enabled: environment.production }),
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
